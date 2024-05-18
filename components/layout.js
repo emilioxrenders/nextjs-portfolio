87f0536cbaeb9f5426b8535489165b6a414236a9
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-05-18 16:37:24 +0200

    init commit

---
+++ components/layout.js
@@ -0,0 +1,3 @@
+export default function Layout({ children }) {
+    return <div className="container py-10 mx-auto flex gap-40 flex-col font-display font-light px-10">{children}</div>;
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      {/* Metadata */}
+      <Head>
+        <title>Emilioxrenders</title>
+      </Head>
+    </Layout>
+  );
+}
