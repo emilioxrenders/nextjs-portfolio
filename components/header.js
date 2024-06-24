6e0e70b2b954eaededbb3e420adf5200c175d8dc
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-06-24 11:54:12 +0200

    Update header.js

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="bg-dark w-full flex justify-center items-center py-4">
       <Link
         href="/"
-        className="text-white w-fit font-extralight tracking-wider"
+        className="text-white w-fit font-light tracking-wider"
       >
         Emilioxrenders
       </Link>
