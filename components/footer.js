91d83c9dd9a0bc9b1971ce0f2cbb34acf44225fa
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-06-24 11:54:01 +0200

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="bg-dark w-full flex justify-center items-center py-4">
-      <p className="text-white w-fit font-extralight text-sm">
+      <p className="text-white w-fit font-light text-sm">
         &copy; Emilio Renders 2024
       </p>
     </div>
