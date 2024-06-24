14967c04392de1d0d3864dca54e6f51d94e72788
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-06-24 11:53:46 +0200

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 export default function Layout({ children, className }) {
   return (
     <div
-      className={`container lg:py-32 py-16 mx-auto flex font-display font-extralight px-10 ${className}`}
+      className={`container lg:py-32 py-16 mx-auto flex font-display font-light px-10 ${className}`}
     >
       {children}
     </div>
