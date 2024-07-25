bd1952fe6fa261d2dde2720066a96d9ddf148ffa
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-07-25 18:42:07 +0200

    updated content

--- pages/index.js
+++ pages/index.js
@@ -26,13 +26,27 @@ export default function Home() {
             className="max-w-sm rounded-sm shadow w-full"
           />
           <div className="flex flex-col text-white gap-10">
-            <h1 className="text-4xl">Exploring the Art of Storytelling</h1>
+            <h1 className="text-4xl">Welcome to my Creative Writing Hub</h1>
             <div className="flex flex-col gap-5">
               <p>
-                Hi there! I'm Emilio, a passionate Creative Writer. Welcome to
-                my portfolio, where I share my perspective on various topics and
-                hopefully provide you with some unique insights. Scroll down to
-                explore my latest articles.
+                Hi there! I'm Emilio, a passionate Creative Writer specializing
+                in crafting engaging content and compelling copy.
+                <br></br>
+                <br></br>
+                This portfolio showcases my skills across various writing
+                projects and offers my unique perspective on different topics.{" "}
+                <br></br>
+                <br></br>
+                Whether you're looking for original articles, persuasive copy,
+                or creative content solutions, you've come to the right place.
+                Dive into my latest work below, or reach out to me directly at{" "}
+                <a
+                  href="mailto:emilioxrenders@gmail.com"
+                  className="relative after:w-0 after:transition-all after:duration-200 after:left-0 after:absolute after:bg-white after:h-px after:hover:w-full after:ease-in-out after:bottom-0"
+                >
+                  emilioxrenders@gmail.com
+                </a>
+                !
               </p>
             </div>
           </div>
@@ -43,18 +57,7 @@ export default function Home() {
       <div className="flex justify-center items-center bg-light">
         <Layout className={"flex-col gap-10"}>
           <div className="flex flex-col gap-10 lg:w-1/2">
-            <h2 className="text-3xl">Explore my Articles</h2>
-            <p>
-              Feel free to connect, share your thoughts, or discuss the world of
-              writing with me at{" "}
-              <a
-                href="mailto:emilioxrenders@gmail.com"
-                className="relative after:w-0 after:transition-all after:duration-200 after:left-0 after:absolute after:bg-black after:h-px after:hover:w-full after:ease-in-out after:bottom-0"
-              >
-                emilioxrenders@gmail.com
-              </a>
-              .
-            </p>
+            <h2 className="text-3xl">Explore my Work</h2>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
@@ -90,6 +93,27 @@ export default function Home() {
             >
               Seasonal Affective Disorder: a Comprehensive Overview
             </Card>
+          </div>
+        </Layout>
+      </div>
+
+      {/* section */}
+      <div className="flex justify-center items-center bg-white">
+        <Layout className={"flex-col gap-10"}>
+          <div className="flex flex-col gap-10 lg:w-1/2">
+            <h2 className="text-3xl">Let's collaborate</h2>
+            <p>
+              Ready to bring your ideas to life? Whether you need captivating
+              articles, persuasive copy, or creative content solutions, I'm here
+              to help. Feel free to connect with me directly at{" "}
+              <a
+                href="mailto:emilioxrenders@gmail.com"
+                className="relative after:w-0 after:transition-all after:duration-200 after:left-0 after:absolute after:bg-black after:h-px after:hover:w-full after:ease-in-out after:bottom-0"
+              >
+                emilioxrenders@gmail.com
+              </a>{" "}
+              and let's start creating something amazing together!
+            </p>
           </div>
         </Layout>
       </div>
