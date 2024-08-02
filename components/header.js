6fce4044ee49f31b2d75ae841b6797820f1e2b7e
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-08-02 12:09:37 +0200

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="bg-dark w-full flex justify-center items-center py-4">
+    <div className="bg-beige w-full flex justify-center items-center py-4">
       <Link
         href="/"
         className="text-white w-fit font-light tracking-wider"
