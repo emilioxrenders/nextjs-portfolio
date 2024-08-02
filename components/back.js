6b52ae0e72e1801d8b32d04e78a923f00e2d8c42
emilioxrenders <115925337+emilioxrenders@users.noreply.github.com>
2024-08-02 12:15:43 +0200

    Update back.js

--- components/back.js
+++ components/back.js
@@ -4,7 +4,7 @@ export default function Back() {
   return (
     <Link
       href="/"
-      className="relative w-fit after:w-0 after:transition-all after:duration-200 after:left-0 after:absolute after:bg-black after:h-px after:hover:w-full after:ease-in-out after:bottom-0"
+      className="relative w-fit after:w-full after:transition-all after:duration-200 after:left-0 after:absolute after:bg-black after:h-px after:hover:w-0 after:ease-in-out after:bottom-0"
     >
       Go back
     </Link>
