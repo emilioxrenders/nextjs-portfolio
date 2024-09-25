4269331a3c42f488405928ca31bbf746af21e895
Emilio Renders <emilioxrenders@Emilios-MacBook-Air.local>
2024-09-25 16:58:30 +0200

    updated header

--- components/header.js
+++ components/header.js
@@ -2,11 +2,8 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="bg-beige w-full flex justify-center items-center py-4">
-      <Link
-        href="/"
-        className="w-fit font-light tracking-wider"
-      >
+    <div className="bg-beige w-full flex justify-center items-center py-4 font-display font-light">
+      <Link href="/" className="w-fit font-light tracking-wider">
         Emilioxrenders
       </Link>
     </div>
